04465ed9893a91f7add00f8ec242f7d3c3ec1b86
Jesus Flores <joy_warmgun@hotmail.com>
2018-02-08 17:30:32 -0600

    Probando insertado 	modified:   src/microservices/dependencia/Dockerfile 	modified:   src/microservices/dependencia/api/models.py 	modified:   src/microservices/dependencia/server/settings.py 	modified:   src/microservices/docker-compose.yml

--- src/microservices/dependencia/api/models.py
+++ src/microservices/dependencia/api/models.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import models
-# from django.contrib.auth.models import AbstractUser
+from mongoengine import *
 
-# class User(AbstractUser):
-    # name = models.CharField(max_length=100, blank=True, null=True)
+class Dependencia(Document):
+    nombre = StringField(max_length=100, required=True)
--- src/microservices/dependencia/server/settings.py
+++ src/microservices/dependencia/server/settings.py
@@ -10,13 +10,16 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
-import os, logging
+from mongoengine import connect
+import os, logging, mongoengine
 
 logging.basicConfig(level=logging.INFO)
 
+if 'DB_HOST' in os.environ and 'DB_NAME' in os.environ:
+    connect(os.environ['DB_NAME'], host=os.environ['DB_HOST'])
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -32,44 +35,51 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    # 'django.contrib.sessions',
-    # 'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'api'
-]
+        # 'django.contrib.admin',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        # 'django.contrib.sessions',
+        # 'django.contrib.messages',
+        'django.contrib.staticfiles',
+        'rest_framework',
+        'api'
+        ]
 
 MIDDLEWARE = [
-    # 'django.middleware.security.SecurityMiddleware',
-    # 'django.contrib.sessions.middleware.SessionMiddleware',
-    # 'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
-    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # 'django.contrib.messages.middleware.MessageMiddleware',
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'middlewares.auth.Auth'
+        # 'django.middleware.security.SecurityMiddleware',
+        # 'django.contrib.sessions.middleware.SessionMiddleware',
+        # 'django.middleware.common.CommonMiddleware',
+        # 'django.middleware.csrf.CsrfViewMiddleware',
+        # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+        # 'django.contrib.messages.middleware.MessageMiddleware',
+        # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+        'middlewares.auth.Auth'
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.AllowAny',
+    ),
+}
 
 ROOT_URLCONF = 'server.urls'
 
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
+        {
+            'BACKEND': 'django.template.backends.django.DjangoTemplates',
+            'DIRS': [],
+            'APP_DIRS': True,
+            'OPTIONS': {
+                'context_processors': [
+                    'django.template.context_processors.debug',
+                    'django.template.context_processors.request',
+                    'django.contrib.auth.context_processors.auth',
+                    'django.contrib.messages.context_processors.messages',
+                    ],
+                },
+            },
+        ]
 
 WSGI_APPLICATION = 'server.wsgi.application'
 
