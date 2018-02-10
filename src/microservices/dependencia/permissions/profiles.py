e794a6639f82adb31a78263e72184a39e0ca25aa
Jesus Flores <joy_warmgun@hotmail.com>
2018-02-09 18:02:40 -0600

    Cambiando protocolo de permisos  On branch development 	modified:   src/microservices/dependencia/permissions/profiles.py

--- src/microservices/dependencia/permissions/profiles.py
+++ src/microservices/dependencia/permissions/profiles.py
@@ -1,14 +1,16 @@
 from rest_framework import permissions
+from django.urls import resolve
 import logging
 
 class ProfilePermission(permissions.BasePermission):
     
     message = 'Recurso no permitido.'
+    admin = 'Administrador'
 
     def has_permission(self, request, view):
 
-        if request.method == 'GET' or request.method == 'POST':
-            if request.path == '/v1/dependencia/' and 'Administrador' in request.profiles:
+        if resolve(request.path_info).url_name == 'main-dependencia' and self.admin in request.profiles:
+            if request.method == 'GET' or request.method == 'POST':
                 return True
 
         return False
