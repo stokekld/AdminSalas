aef9b462b8c4de0013589fc7a5441094e4a4e971
Jesus Flores <joy_warmgun@hotmail.com>
2018-02-20 20:16:26 -0600

    Comenzando test  On branch development 	modified:   src/microservices/dependencia/api/tests.py

--- src/microservices/dependencia/api/tests.py
+++ src/microservices/dependencia/api/tests.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.test import TestCase
+from django.test import SimpleTestCase, Client
 
-# Create your tests here.
+class TestMicroservice(SimpleTestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_nologin(self):
+        response = self.client.get('/v1/dependencia/')
+        self.assertEqual(response.status_code, 401)
+        response = self.client.post('/v1/dependencia/')
+        self.assertEqual(response.status_code, 401)
