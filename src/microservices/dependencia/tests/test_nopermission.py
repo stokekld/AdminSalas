e4e191cb061483f15270b8d85b65c58b40bea4da
Jesus Flores <joy_warmgun@hotmail.com>
2018-02-22 19:44:36 -0600

    Agregando modulo de test  On branch development 	deleted:    src/microservices/dependencia/api/tests.py 	new file:   src/microservices/dependencia/tests/__init_.py 	new file:   src/microservices/dependencia/tests/test_nologin.py 	new file:   src/microservices/dependencia/tests/test_nopermission.py

---
+++ src/microservices/dependencia/tests/test_nopermission.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import SimpleTestCase, Client
+from django.conf import settings
+import random, jwt
+
+class TestMicroservice(SimpleTestCase):
+    def setUp(self):
+        self.id = ''.join([random.SystemRandom().choice('abcdef0123456789') for i in range(24)])
+        self.client = Client()
+        self.token = jwt.encode({
+            "perfiles": ["Supervisor", "Solicitante", "Observador", "Analista"],
+            "activo": True
+        }, settings.SECRET_KEY, algorithm='HS256')
+
+    def test_nopermission(self):
+        response = self.client.get('/v1/dependencia/', HTTP_TOKEN=self.token)
+        self.assertEqual(response.status_code, 403)
+        response = self.client.post('/v1/dependencia/', HTTP_TOKEN=self.token)
+        self.assertEqual(response.status_code, 403)
+
+        # El endpoint /v1/dependencia/ lo puede ocupar cualquiera logeado
+        response = self.client.post('/v1/dependencia/' + self.id, HTTP_TOKEN=self.token)
+        self.assertEqual(response.status_code, 403)
+        response = self.client.put('/v1/dependencia/' + self.id, HTTP_TOKEN=self.token)
+        self.assertEqual(response.status_code, 403)
+        response = self.client.delete('/v1/dependencia/' + self.id, HTTP_TOKEN=self.token)
+        self.assertEqual(response.status_code, 403)
