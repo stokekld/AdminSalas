1b3d0ec2eb985c3ec5ae0d24d357ae0967820a93
Jesus Flores <joy_warmgun@hotmail.com>
2018-03-06 19:20:02 -0600

    Completando el test  On branch development 	modified:   src/microservices/usuario/requirements.txt 	modified:   src/microservices/usuario/tests/test_postUser.py

--- src/microservices/usuario/tests/test_postUser.py
+++ src/microservices/usuario/tests/test_postUser.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 from django.test import SimpleTestCase, Client
 from django.conf import settings
-import random, jwt, logging
+import random, jwt, logging, requests, json
 
 class TestMicroservice(SimpleTestCase):
     def setUp(self):
@@ -15,10 +15,13 @@ class TestMicroservice(SimpleTestCase):
 
     def test_postUser(self):
 
-        response = self.client.post('/v1/dependencia/', {"nombre":"Dependencia"}, content_type='application/json', HTTP_TOKEN=self.admin)
+        response = requests.post('http://apigw/v1/dependencia/', data=json.dumps({"nombre":"Dependencia"}), headers={
+            'content-type': 'application/json',
+            'token': self.admin
+        })
         self.assertEqual(response.status_code, 201)
 
-        id = response.data['id']
+        id = json.loads(response.content)["id"]
 
         response = self.client.post('/v1/usuario', {
             "datos": {
@@ -35,7 +38,10 @@ class TestMicroservice(SimpleTestCase):
         }, content_type='application/json', HTTP_TOKEN=self.admin)
         self.assertEqual(response.status_code, 201)
 
-        response = self.client.delete('/v1/dependencia/' + id, HTTP_TOKEN=self.admin)
+        response = requests.delete('http://apigw/v1/dependencia/' + id, headers={
+            'content-type': 'application/json',
+            'token': self.admin
+        })
         self.assertEqual(response.status_code, 200)
 
 
