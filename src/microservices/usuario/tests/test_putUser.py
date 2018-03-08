debaa463c84eaf90140f3c8110b26e42566bf4c2
Jesus Flores <joy_warmgun@hotmail.com>
2018-03-08 12:25:01 -0600

    Agregando test de actualizacion de usuario  On branch development 	new file:   src/microservices/usuario/tests/test_putUser.py

---
+++ src/microservices/usuario/tests/test_putUser.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import SimpleTestCase, Client
+from django.conf import settings
+import random, jwt, logging, requests, json
+
+class TestMicroservice(SimpleTestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin = jwt.encode({
+            "perfiles": ["Administrador"],
+            "activo": True
+        }, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+
+    def test_postUser(self):
+
+        response = requests.post('http://apigw/v1/dependencia/', data=json.dumps({"nombre":"Dependencia"}), headers={
+            'content-type': 'application/json',
+            'token': self.admin
+        })
+        self.assertEqual(response.status_code, 201)
+
+        id = json.loads(response.content)["id"]
+
+        data = {
+            "datos": {
+                "nombre": "Ricardo",
+                "apaterno": "Guerra",
+                "amaterno": "Suarez",
+                "email": "ejemplo@ejemplo.com",
+                "telefono": "Ext: 12345"
+            },
+            "dependencia": id,
+            "perfiles": ["Supervisor", "Solicitante", "Observador", "Analista"],
+            "user": "rootroot",
+            "password": "123456",
+            "activo": True
+        }
+
+        response = self.client.post('/v1/usuario/', json.dumps(data), content_type='application/json', HTTP_TOKEN=self.admin)
+        self.assertEqual(response.status_code, 201)
+
+        
+        response = self.client.put('/v1/usuario/', json.dumps(data), content_type='application/json', HTTP_TOKEN=self.admin)
+        self.assertEqual(response.status_code, 200)
+
+
