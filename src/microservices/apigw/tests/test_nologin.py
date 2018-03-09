ba4ff28a808965e23d4dc3760f32552144c443e9
Jesus Flores <joy_warmgun@hotmail.com>
2018-03-08 19:34:49 -0600

    Agrgando tokens de error  On branch apigw-v2 	modified:   src/microservices/apigw/tests/test_nologin.py

--- src/microservices/apigw/tests/test_nologin.py
+++ src/microservices/apigw/tests/test_nologin.py
@@ -3,16 +3,47 @@ from __future__ import unicode_literals
 
 from django.test import SimpleTestCase, Client
 from django.conf import settings
+from datetime import datetime, timedelta
 import jwt
 
 class TestMicroservice(SimpleTestCase):
     def setUp(self):
-        self.token = jwt.encode({
-                "perfiles": ["Administrador", "Supervisor", "Solicitante", "Observador", "Analista"],
+        self.tokens = [
+            {
+                "exp": datetime.utcnow(),
+                "id": "1234455678890",
+                "perfiles": [],
+                "activo": True
+            },
+            {
+                "exp": datetime.utcnow() + timedelta(seconds=60),
+                "perfiles": [],
+                "activo": True
+            },
+            {
+                "exp": datetime.utcnow() + timedelta(seconds=60),
+                "id": "1234455678890",
+                "activo": True
+            },
+            {
+                "exp": datetime.utcnow() + timedelta(seconds=60),
+                "id": "1234455678890",
+                "perfiles": []
+            },
+            {
+                "exp": datetime.utcnow() + timedelta(seconds=60),
+                "id": "1234455678890",
+                "perfiles": [],
                 "activo": False
-            }, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+            }
+        ]
         self.client = Client()
 
     def test_nologin(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 401)
+
+        for token in self.tokens:
+            hash = jwt.encode(token, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
+            response = self.client.get('/', HTTP_TOKEN=hash)
+            self.assertEqual(response.status_code, 401)
