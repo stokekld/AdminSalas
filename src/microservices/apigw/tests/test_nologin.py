b89f52b0eab26164c6f1af9c9e9600b07f0fb09e
Jesus Flores <joy_warmgun@hotmail.com>
2018-03-08 19:50:46 -0600

    Modificando cuando expira el token  On branch apigw-v2 	modified:   apigw/tests/test_nologin.py

--- src/microservices/apigw/tests/test_nologin.py
+++ src/microservices/apigw/tests/test_nologin.py
@@ -4,13 +4,13 @@ from __future__ import unicode_literals
 from django.test import SimpleTestCase, Client
 from django.conf import settings
 from datetime import datetime, timedelta
-import jwt
+import jwt, logging
 
 class TestMicroservice(SimpleTestCase):
     def setUp(self):
         self.tokens = [
             {
-                "exp": datetime.utcnow(),
+                "exp": datetime.utcnow() - timedelta(seconds=60),
                 "id": "1234455678890",
                 "perfiles": [],
                 "activo": True
@@ -44,6 +44,7 @@ class TestMicroservice(SimpleTestCase):
         self.assertEqual(response.status_code, 401)
 
         for token in self.tokens:
+            logging.info(token)
             hash = jwt.encode(token, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
             response = self.client.get('/', HTTP_TOKEN=hash)
             self.assertEqual(response.status_code, 401)
