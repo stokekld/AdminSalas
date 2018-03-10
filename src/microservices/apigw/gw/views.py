5d944aef38de59289adffd82e6784f5b8bb22221
Jesus Flores <joy_warmgun@hotmail.com>
2018-03-10 14:57:49 -0600

    Primera version de request del apigw  On branch apigw-v2 	modified:   src/microservices/apigw/gw/views.py

--- src/microservices/apigw/gw/views.py
+++ src/microservices/apigw/gw/views.py
@@ -1,11 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import QueryDict
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-import logging
+import requests, logging
 
-@api_view()
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 def general(request, version, microservice, path, format=None):
-    return Response({"hola":"mundo"})
+
+    port = 8080
+
+    url = ('http://%s:%s/%s/%s%s') % (microservice, port, version, path, '' if request.META['QUERY_STRING'] == '' else '?' + request.META['QUERY_STRING'])
+
+    response = requests.request(request.method.lower(), url, params=QueryDict(request.META['QUERY_STRING']).dict(), data=request.body, headers={'content-type': request.content_type})
+
+    return Response(response.text, response.status_code, headers=response.headers)
