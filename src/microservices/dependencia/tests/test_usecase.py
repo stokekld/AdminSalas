920cd0b8ee55311e1625a34112b834ddca027e6a
Jesus Flores <joy_warmgun@hotmail.com>
2018-03-16 13:45:13 -0600

    Agregando siglas de dependencia al test  On branch development 	modified:   src/microservices/dependencia/tests/test_usecase.py

--- src/microservices/dependencia/tests/test_usecase.py
+++ src/microservices/dependencia/tests/test_usecase.py
@@ -3,14 +3,19 @@ from __future__ import unicode_literals
 
 from django.test import SimpleTestCase, Client
 from django.conf import settings
-import random, jwt, logging
+import random, jwt, logging, json
 
 class TestMicroservice(SimpleTestCase):
     def setUp(self):
         self.client = Client()
 
     def test_crudDep(self):
-        response = self.client.post("/v1/", '{"nombre":"nombre1"}', content_type='application/json')
+        dependencia = {
+            "nombre": "nombre1",
+            "siglas": "siglas1"
+        }
+
+        response = self.client.post("/v1/", json.dumps(dependencia), content_type='application/json')
         self.assertEqual(response.status_code, 201)
 
         id = response.data['id']
@@ -18,7 +23,12 @@ class TestMicroservice(SimpleTestCase):
         response = self.client.get('/v1/' + id)
         self.assertEqual(response.status_code, 200)
 
-        response = self.client.put('/v1/' + id, '{"nombre":"nombre2"}', content_type='application/json')
+        dependencia = {
+            "nombre": "nombre2",
+            "siglas": "siglas2"
+        }
+
+        response = self.client.put('/v1/' + id, json.dumps(dependencia), content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
         response = self.client.delete('/v1/' + id)
