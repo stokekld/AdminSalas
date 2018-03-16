5ba8362726e9cb31498f342c2ef9820747c02010
Jesus Flores <joy_warmgun@hotmail.com>
2018-03-16 12:49:28 -0600

    Cambiando url de usuario  On branch apigw-v2 	modified:   src/microservices/usuario/api/urls.py

--- src/microservices/usuario/api/urls.py
+++ src/microservices/usuario/api/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from api import views
 
 urlpatterns = [
-    url(r'v1/usuario/$', views.MainView.as_view(), name='main-usuario'),
-    url(r'v1/usuario/(?P<id>[0-9a-z]{24})$', views.IdView.as_view(), name='id-usuario')
+    url(r'v1/$', views.MainView.as_view(), name='main-usuario'),
+    url(r'v1/(?P<id>[0-9a-z]{24})$', views.IdView.as_view(), name='id-usuario')
 ]
 
