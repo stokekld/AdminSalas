b808d89299e60deca830752170d5e00d13efdead
Jesus Flores <joy_warmgun@hotmail.com>
2018-03-16 13:30:40 -0600

    Cambiando test a solo caso de uso  On branch development 	modified:   src/microservices/dependencia/tests/test_usecase.py

--- src/microservices/dependencia/tests/test_usecase.py
+++ src/microservices/dependencia/tests/test_usecase.py
@@ -8,39 +8,23 @@ import random, jwt, logging
 class TestMicroservice(SimpleTestCase):
     def setUp(self):
         self.client = Client()
-        self.admin = jwt.encode({
-            "perfiles": ["Administrador"],
-            "activo": True
-        }, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
-        self.user= jwt.encode({
-            "perfiles": ["Supervisor", "Solicitante", "Observador", "Analista"],
-            "activo": True
-        }, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
 
     def test_crudDep(self):
-        response = self.client.post("/v1/dependencia/", '{"nombre":"nombre1"}', content_type='application/json', HTTP_TOKEN=self.admin)
+        response = self.client.post("/v1/", '{"nombre":"nombre1"}', content_type='application/json')
         self.assertEqual(response.status_code, 201)
 
         id = response.data['id']
 
-        response = self.client.get('/v1/dependencia/' + id, HTTP_TOKEN=self.user)
-        self.assertEqual(response.status_code, 200)
-        response = self.client.get('/v1/dependencia/' + id, HTTP_TOKEN=self.admin)
+        response = self.client.get('/v1/' + id)
         self.assertEqual(response.status_code, 200)
 
-        response = self.client.put('/v1/dependencia/' + id, HTTP_TOKEN=self.user)
-        self.assertEqual(response.status_code, 403)
-        response = self.client.put('/v1/dependencia/' + id, '{"nombre":"nombre2"}', content_type='application/json', HTTP_TOKEN=self.admin)
+        response = self.client.put('/v1/' + id, '{"nombre":"nombre2"}', content_type='application/json')
         self.assertEqual(response.status_code, 200)
 
-        response = self.client.delete('/v1/dependencia/' + id, HTTP_TOKEN=self.user)
-        self.assertEqual(response.status_code, 403)
-        response = self.client.delete('/v1/dependencia/' + id, HTTP_TOKEN=self.admin)
+        response = self.client.delete('/v1/' + id)
         self.assertEqual(response.status_code, 200)
 
 
     def test_getAll(self):
-        response = self.client.get('/v1/dependencia/', HTTP_TOKEN=self.admin)
+        response = self.client.get('/v1/')
         self.assertEqual(response.status_code, 200)
-        response = self.client.get('/v1/dependencia/', HTTP_TOKEN=self.user)
-        self.assertEqual(response.status_code, 403)
