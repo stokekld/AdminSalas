5e8f121a830e94386ff3fba37bebce5f4867d3b6
Jesus Flores <joy_warmgun@hotmail.com>
2018-03-21 17:32:26 -0600

    Agregando graphene-mongo  On branch development 	modified:   src/microservices/dependencia/api/schema.py 	modified:   src/microservices/dependencia/requirements.txt

--- src/microservices/dependencia/api/schema.py
+++ src/microservices/dependencia/api/schema.py
@@ -1,15 +1,24 @@
+from graphene_mongo import MongoengineObjectType
 from .models import Dependencia
-import graphene
+import graphene, logging
 
-class DependenciaSchema(graphene.ObjectType):
-    id = graphene.String()
-    nombre = graphene.String() 
-    siglas = graphene.String()
+class DependenciaType(MongoengineObjectType):
+    class Meta:
+        model = Dependencia
 
 class Query(graphene.ObjectType):
-    dependencia = graphene.List(DependenciaSchema)
+    dependencia = graphene.Field(DependenciaType, id=graphene.String())
+    dependencias = graphene.List(DependenciaType)
 
-    def resolve_dependencia(self, info):
+    def resolve_dependencia(self, info, **kwargs):
+        id = kwargs.get('id')
+
+        if id is not None:
+            return Dependencia.objects.get(id=id)
+
+        return None
+
+    def resolve_dependencias(self, info, **kwargs):
         return Dependencia.objects.all()
 
 schema = graphene.Schema(query=Query)
