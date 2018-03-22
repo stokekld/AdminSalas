ba1bfa07853327319078345b4bbfb1b023063595
Jesus Flores <joy_warmgun@hotmail.com>
2018-03-22 14:16:22 -0600

    Agregando propiedad unique a siglas  On branch development 	modified:   src/microservices/dependencia/api/models.py

--- src/microservices/dependencia/api/models.py
+++ src/microservices/dependencia/api/models.py
@@ -5,4 +5,4 @@ from mongoengine import *
 
 class Dependencia(Document):
     nombre = StringField(max_length=100, required=True)
-    siglas = StringField(max_length=10, required=True)
+    siglas = StringField(max_length=10, required=True, unique=True)
