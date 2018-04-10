4c501f3167b8101743c5b436352fc4037fb45e10
Jesus Flores <joy_warmgun@hotmail.com>
2018-04-09 19:27:46 -0500

    Creando test para creacion de token  On branch development 	new file:   src/microservices/usuario/tests/test_authUser.py

---
+++ src/microservices/usuario/tests/test_authUser.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import SimpleTestCase, Client
+from django.conf import settings
+import random, jwt, logging, requests, json, string
+
+class TestMicroservice(SimpleTestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_postUser(self):
+        dependencia = {
+            "nombre": ''.join(random.choice(string.ascii_lowercase) for _ in range(100)),
+            "siglas": ''.join(random.choice(string.ascii_lowercase) for _ in range(10))
+        }
+
+        response = requests.post('http://dependencia:8080/v1/', data=json.dumps(dependencia), headers={
+            'content-type': 'application/json',
+        })
+        self.assertEqual(response.status_code, 201)
+
+        id = json.loads(response.content)["id"]
+
+        data = {
+            "datos": {
+                "nombre": "Ricardo",
+                "apaterno": "Guerra",
+                "amaterno": "Suarez",
+                "email": "ejemplo@ejemplo.com",
+                "telefono": "Ext: 12345"
+            },
+            "dependencia": id,
+            "perfiles": ["Supervisor", "Solicitante", "Observador", "Analista"],
+            "user": ''.join(random.choice(string.ascii_lowercase) for _ in range(10)),
+            "password": "123456",
+            "activo": True
+        }
+
+        response = self.client.post('/v1/', json.dumps(data), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+        credentials = {
+            "user": data["user"],
+            "password": data["password"]
+        }
+
+        response = self.client.post('/v1/auth', json.dumps(credentials), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+        response = requests.delete('http://dependencia:8080/v1/' + id, headers={
+            'content-type': 'application/json',
+        })
+        self.assertEqual(response.status_code, 200)
