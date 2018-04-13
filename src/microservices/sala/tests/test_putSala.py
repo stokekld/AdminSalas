cd986df5f9a7251cbe66d747a9cff3505af9ebe4
Jesus Flores <joy_warmgun@hotmail.com>
2018-04-13 17:39:44 -0500

    Agregando test para metodo put sala  On branch development 	new file:   src/microservices/sala/tests/test_putSala.py

---
+++ src/microservices/sala/tests/test_putSala.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import SimpleTestCase, Client
+from django.conf import settings
+import random, logging, requests, json, string
+
+class TestMicroservice(SimpleTestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_putUser(self):
+        sala = {
+            "nombre": ''.join(random.choice(string.ascii_lowercase) for _ in range(10)),
+            "capNormal": 100,
+            "capMax": 100
+        }
+
+        response = self.client.post('/v1/', json.dumps(sala), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+        id = json.loads(response.content)["id"]
+
+        response = self.client.put('/v1/' + id  , json.dumps(sala), content_type='application/json')
+        self.assertEqual(response.status_code, 200)
+
