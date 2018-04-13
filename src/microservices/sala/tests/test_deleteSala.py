c51d45e5fa70cf8b9da132b1b88fb910dc35843d
Jesus Flores <joy_warmgun@hotmail.com>
2018-04-13 18:04:14 -0500

    Agregando test para metodo delete sala  On branch development 	new file:   sala/tests/test_deleteSala.py

---
+++ src/microservices/sala/tests/test_deleteSala.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.test import SimpleTestCase, Client
+from django.conf import settings
+import random, logging, requests, json, string
+
+class TestMicroservice(SimpleTestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_postUser(self):
+        sala = {
+            "nombre": ''.join(random.choice(string.ascii_lowercase) for _ in range(10)),
+            "capNormal": 100,
+            "capMax": 100
+        }
+
+        response = self.client.post('/v1/', json.dumps(sala), content_type='application/json')
+        self.assertEqual(response.status_code, 201)
+
+        id = json.loads(response.content)["id"]
+
+        response = self.client.delete('/v1/' + id, content_type='application/json')
+        self.assertEqual(response.status_code, 200)
